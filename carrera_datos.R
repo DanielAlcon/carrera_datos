59e6e7a2abccf0d9defd9e58c1fd605f113422b9
DanielAlcon <daniel.alcon.diaz@gmail.com>
2019-05-26 17:50:43 +0200

    require data.table

--- carrera_datos.R
+++ carrera_datos.R
@@ -1,8 +1,8 @@
 # comando para descarga de ficheros completa en bash
 # gsutil -m cp gs://securitas/*.csv data/
-#install.packages("data.table")
+require("data.table")
 # TODO: cambiar directorior de trabajo a donde se encuentren los ficheros
-#setwd("./data")
+setwd("./data")
 
 temp = list.files(pattern="natalidad*")
 race = read.csv("./race.csv", header = T, sep = ",")
@@ -16,23 +16,26 @@ to_kgs = function(value) {
 
 library(data.table)
 
-data_total = rbindlist(lapply(list.files(path=".", pattern="natalidad*"), data.table::fread , header = T, verbose = T, sep = ',', select = c("source_year", "state", "is_male", "child_race", "weight_pounds")))
+data_total = rbindlist(lapply(list.files(path=".", pattern="natalidad*"), data.table::fread , header = T, verbose = F, sep = ',', select = c("source_year", "state", "is_male", "child_race", "weight_pounds")))
 
 # Total nacimientos por décadas y estado
 b70 = setnames(data_total[source_year %between% c(1970,1979), .N, keyby = state],"N","b70")
 b80 = setnames(data_total[source_year %between% c(1980,1989), .N, keyby = state],"N","b80")
 b90 = setnames(data_total[source_year %between% c(1990,1999), .N, keyby = state],"N","b90")
 b00 = setnames(data_total[source_year %between% c(2000,2010), .N, keyby = state],"N","b00")
+# check for empty "" states
 
 # Raza con mayor nacimiento por década y estado
 r70 = data_total[source_year %between% c(1970,1979), .(r70=max(child_race)), keyby = state]
 r80 = data_total[source_year %between% c(1980,1989), .(r80=max(child_race)), keyby = state]
 r90 = data_total[source_year %between% c(1990,1999), .(r90=max(child_race)), keyby = state]
 r00 = data_total[source_year %between% c(2000,2010), .(r00=max(child_race)), keyby = state]
+# check for empty "" states
 
 # Total nacimientos por sexo y estado
 male = data_total[source_year %between% c(1970,2010), .(male=sum(is_male==TRUE)), keyby = state]
 female = data_total[source_year %between% c(1970,2010), .(female=sum(is_male==FALSE)), keyby = state]
+# check for empty "" states
 
 # Pesos medios por estado
 #Weight: peso medio en kilos de todos los niños nacidos en ese estado desde el 70 al 2010 (float)
